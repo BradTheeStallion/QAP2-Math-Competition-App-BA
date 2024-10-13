ff5638ce03ce4279c29981fb75f63a555fde980c
BradTheeStallion <bradley.ayers@keyin.com>
2024-10-13 13:31:59 -0230

    Made math problems a little more robust. I had keept them simple to do them in my head more easily. I also had one problem ask me to divide by zero so I fixed that.

--- utils/mathUtilities.js
+++ utils/mathUtilities.js
@@ -1,35 +1,39 @@
 function getQuestion() {
-    const num1 = Math.floor(Math.random() * 10);
-    const num2 = Math.floor(Math.random() * 10);
     const operators = ['+', '-', '*', '/'];
     const operator = operators[Math.floor(Math.random() * operators.length)];
-
-    let question;
-    let answer;
+    let num1, num2, question, answer;
 
     switch (operator) {
         case '+':
+            num1 = Math.floor(Math.random() * 50) + 1;
+            num2 = Math.floor(Math.random() * 50) + 1;
             question = `${num1} + ${num2}`;
             answer = num1 + num2;
             break;
         case '-':
+            num1 = Math.floor(Math.random() * 50) + 26; // Ensure num1 is always larger
+            num2 = Math.floor(Math.random() * 25) + 1;
             question = `${num1} - ${num2}`;
             answer = num1 - num2;
             break;
         case '*':
+            num1 = Math.floor(Math.random() * 12) + 1;
+            num2 = Math.floor(Math.random() * 12) + 1;
             question = `${num1} * ${num2}`;
             answer = num1 * num2;
             break;
         case '/':
+            num2 = Math.floor(Math.random() * 12) + 1; // Divisor (1 to 12)
+            answer = Math.floor(Math.random() * 12) + 1; // Quotient (1 to 12)
+            num1 = num2 * answer; // Ensure division results in a whole number
             question = `${num1} / ${num2}`;
-            answer = num2 === 0 ? 'undefined' : parseFloat((num1 / num2).toFixed(2));
             break;
     }
 
     return { question, answer };
 }
 
-function isCorrectAnswer(question, answer) {
+function isCorrectAnswer(question, userAnswer) {
     const parts = question.split(' ');
     const num1 = parseInt(parts[0]);
     const num2 = parseInt(parts[2]);
@@ -47,14 +51,14 @@ function isCorrectAnswer(question, answer) {
             correctAnswer = num1 * num2;
             break;
         case '/':
-            correctAnswer = num2 === 0 ? 'undefined' : parseFloat((num1 / num2).toFixed(2));
+            correctAnswer = num1 / num2;
             break;
     }
 
-    return correctAnswer === answer;
+    return Math.abs(correctAnswer - userAnswer) < 0.01; // Allow for small floating-point errors
 }
 
 module.exports = {
     getQuestion,
     isCorrectAnswer
-}
+};
